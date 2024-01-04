4745e97a908ad66190638fbd4cce1c9bb6e280d7
Oleh Lunev <129771696+OlehLunov@users.noreply.github.com>
2024-01-04 16:11:47 +0200

    some fixes

--- weatherAPP/main.js
+++ weatherAPP/main.js
@@ -10,7 +10,7 @@ async function checkWeather(city){
 
     if (response.status == 404){
         document.querySelector(".error").style.display = "block";
-        document.querySelector(".weathe").style.display = "none";
+        document.querySelector(".weather").style.display = "none";
     } else {
         var data = await response.json();
 
@@ -34,11 +34,10 @@ async function checkWeather(city){
         else if (data.weather[0].main == "Mist"){
             weatherIcon.src = "images/mist.png";
         }
-    
+        document.querySelector(".weather").style.display = "block";
+        document.querySelector(".error").style.display = "none";
     }
 
-    document.querySelector(".weather").style.display = "block";
-    document.querySelector(".error").style.display = "none";
 }
 
 btn.addEventListener("click", () => {
